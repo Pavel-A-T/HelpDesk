a67be3cf12a60e755241dbad0b9e374e2c19df4d
Pavel Titov <otrepiev.grishka@yandex.ru>
2021-12-10 19:40:14 +0300

    CORS

--- src/server.js
+++ src/server.js
@@ -10,6 +10,42 @@ const router = new Router();
 const path = './src/db.json';
 const fileDB = fileSystem.readFileSync(path);
 
+app.use(async (ctx, next) => {
+  const origin = ctx.request.get('Origin');
+  if (!origin) {
+    return await next();
+  }
+
+  const headers = { 'Access-Control-Allow-Origin': '*' };
+
+  if (ctx.request.method !== 'OPTIONS') {
+    ctx.response.set({ ...headers });
+    try {
+      return await next();
+    } catch (e) {
+      e.headers = { ...e.headers, ...headers };
+      throw e;
+    }
+  }
+
+  if (ctx.request.get('Access-Control-Request-Method')) {
+    ctx.response.set({
+      ...headers,
+      'Access-Control-Allow-Methods': 'GET, POST, DELETE',
+    });
+
+    if (ctx.request.get('Access-Control-Request-Headers')) {
+      ctx.response.set(
+          'Access-Control-Allow-Headers',
+          ctx.request.get('Access-Control-Request-Headers'),
+      );
+    }
+
+    ctx.response.status = 204;
+  }
+});
+
+
 router.get('/allTickets', async (ctx) => {
   try {
     const dataFromDB = JSON.parse(fileDB);
