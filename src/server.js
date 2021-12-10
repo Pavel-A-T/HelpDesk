079cd68378ea5e1329d4630d630b6ca1831818b5
Pavel Titov <otrepiev.grishka@yandex.ru>
2021-12-10 19:16:22 +0300

    закончил логику API

--- src/server.js
+++ src/server.js
@@ -1,38 +1,84 @@
 const Koa = require('koa');
-const KoaBody = require('koa-body');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 const fileSystem = require('fs');
-const config = require('./config')
-
+const config = require('./config');
 
 const app = new Koa();
 const router = new Router();
 
 const path = './src/db.json';
 const fileDB = fileSystem.readFileSync(path);
-const dataFromDB = JSON.parse(fileDB);
 
-router.get('/', ctx => {
-    const fileDB = fileSystem.readFileSync(path);
-    let dataFromDB = JSON.parse(fileDB);
-    dataFromDB.map(i => delete i.description);
+router.get('/allTickets', async (ctx) => {
+  try {
+    const dataFromDB = JSON.parse(fileDB);
+    dataFromDB.map((item) => delete item.description);
     ctx.body = dataFromDB;
+  } catch (error) {
+    console.error('err', error);
+    ctx.status = 500;
+    ctx.body = 'Internal Server error';
+  }
 });
 
-router.post('/', ctx => {
-    console.log(ctx.params);
+router.get('/:id', async (ctx) => {
+  try {
+    const { id } = Number(ctx.params);
+    if (Number.isNaN(id) || !Number.isInteger(id) || id < 0) throw new Error(" incorrect 'id'");
+    const dataFromDB = JSON.parse(fileDB);
+    ctx.body = dataFromDB.filter((i) => i.id === id);
+  } catch (error) {
+    console.error('err', error);
+    ctx.status = 500;
+    ctx.body = 'Internal Server error';
+  }
+});
+
+router.post('/', async (ctx) => {
+  try {
     const data = ctx.request.body;
-    fileSystem.writeFileSync(path, JSON.stringify([...dataFromDB, data]));
+    const dataFromDB = JSON.parse(fileDB);
+    let result;
+    if (Object.keys(data).length !== 4) throw new Error(' incorrect object. The number of object keys is not equal to four');
+    if (typeof data.name === 'undefined') throw new Error(" incorrect object. Key 'name' not found");
+    if (typeof data.description === 'undefined') throw new Error(" incorrect object. Key 'description' not found");
+    if (typeof data.status === 'undefined') throw new Error(" incorrect object. Key 'status' not found");
+    if (typeof data.created === 'undefined') throw new Error(" incorrect object. Key 'created' not found");
+    if (dataFromDB && dataFromDB.length > 0) {
+      dataFromDB.sort((a, b) => b.id - a.id);
+      result = { id: dataFromDB[0].id + 1, ...data };
+    } else result = { id: 1, ...data };
+    fileSystem.writeFileSync(path, JSON.stringify([...dataFromDB, result]));
     ctx.body = data;
+  } catch (error) {
+    console.error('err', error);
+    ctx.status = 500;
+    ctx.body = 'Internal Server error';
+  }
+});
+
+router.delete('/:id', async (ctx) => {
+  try {
+    const { id } = Number(ctx.params);
+    if (Number.isNaN(id) || !Number.isInteger(id) || id < 0) throw new Error(" incorrect 'id'");
+    const dataFromDB = JSON.parse(fileDB);
+    const index = dataFromDB.findIndex((i) => i.id === id);
+    if (index > -1) dataFromDB.splice(index, 1);
+    fileSystem.writeFileSync(path, JSON.stringify([...dataFromDB]));
+    ctx.body = true;
+  } catch (error) {
+    console.error('err', error);
+    ctx.status = 500;
+    ctx.body = 'Internal Server error';
+  }
 });
 
 app.use(bodyParser());
 app.use(router.routes());
 app.use(router.allowedMethods());
-app.listen(config.port, () => {console.log(`Server started on port ${config.port}`)});
-
-
-
-
-
+app.listen(config.port, () => {
+  app.use(async (ctx) => {
+    ctx.body = `Server started on port ${config.port}`;
+  });
+});
